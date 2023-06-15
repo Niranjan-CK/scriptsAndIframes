d5dbe55aa483d0b6a825bd34c04250a34b29700c
Niranjanck <niranjankkv1999@gmail.com>
2023-06-15 11:34:58 +0530

    final

--- Index.php
+++ Index.php
@@ -1,119 +1,60 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            Scripts and Iframes
-        </title>
-    </head>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    <?php
-    
-    if($_SERVER['REQUEST_METHOD']=="POST")
-    {
-        $checkedItems = $_POST['handleScripts'];
-        $url = $_POST['url'];
-        if(!empty($url))
+    $url = $_POST['url'];
+
+    $html = file_get_contents($url);
+
+    $searchData = $_POST['links'];
+    if (!empty($searchData)) {
+        foreach($searchData as $search)
         {
-            $host = parse_url($url);
-            if($host['host']!='')
+            $insideScript = '/(<script\b[^>]*>)(.*?(' . preg_quote($search, '/') . ').*?)(<\/script>)/is'; 
+            if(empty($insideScript))
             {
-            ?>
-            <iframe width="720" height="405"
-                    src="<?= $url ?>" frameborder="2"></iframe>
-            <?php
+                $html = preg_replace($insideScript,  '', $html); 
             }
             else
             {
-                if(str_contains($url,'<script>'))
+                $iframePattern = '/(<iframe[^>]*src=[\'"])(.*?)(' . preg_quote($search, '/') . ')(.*?[\'"])/is'; 
+                if(empty($iframePattern))
                 {
-                    echo $url;
+                    $outsideScript = '/(<script[^>]*src=[\'"])(.*?)(' . preg_quote($search, '/') . ')(.*?[\'"])/is'; 
+                    $html = preg_replace($outsideScript,  '', $html); 
                 }
-                else
-                {
-                    echo "nothing to show";
+                else{
+
+                    $html = preg_replace($iframePattern, '$1"' . '' . '"', $html); // Perform the replacement
+
+
                 }
             }
-
 
             
-
         }
-
-        if(!empty($checkedItems))
-        {
-            foreach($checkedItems as $item)
-            {
-                switch($item)
-                {
-                    case "ads" : echo "ads";?>
-                                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                        <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px"
-                                            data-ad-client="ca-pub-1234567890123456" data-ad-slot="1234567890"></ins>
-                                        <script>
-                                            (adsbygoogle = window.adsbygoogle || []).push({});
-                                        </script>
-                                        <?php
-                                    break ;
-                    case "analytics" :echo "analytics";?>
-                                        <script>
-                                            
-                                            (function (i, s, o, g, r, a, m) {
-                                                i['GoogleAnalyticsObject'] = r;
-                                                i[r] = i[r] || function () {
-                                                    (i[r].q = i[r].q || []).push(arguments)
-                                                }, i[r].l = 1 * new Date();
-                                                a = s.createElement(o),
-                                                    m = s.getElementsByTagName(o)[0];
-                                                a.async = 1;
-                                                a.src = g;
-                                                m.parentNode.insertBefore(a, m)
-                                            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-                                            ga('create', 'UA-XXXXX-Y', 'auto');
-                                            ga('send', 'pageview');
-                                        </script>
-                                    <?php 
-                                        break;
-                    case "sound" : echo "sound" ;?>
-                                    <script src="https://connect.soundcloud.com/sdk/sdk-3.3.2.js"></script>
-                                    <script>
-                                        SC.initialize({
-                                            client_id: 'YOUR_CLIENT_ID',
-                                            redirect_uri: 'https://example.com/callback'
-                                        });
-                                        SC.connect().then(function () {
-                                            return SC.get('/me');
-                                        }).then(function (me) {
-                                            alert('Hello, ' + me.username);
-                                        });
-                                    </script>
-                                    <?php
-                                    break;
-                    case "youtube" : echo "youtube"; ?>
-                                    <iframe id="ytplayer" type="text/html" width="720" height="405"
-                                    src="https://www.youtube.com/embed/MswordkJksdNm7lc1UVf-VE" frameborder="0" allowfullscreen>
-                                    <?php
-                                    break ;
-                }
-            }
-        }
-        
     }
+    echo $html;
+}
+?>
 
-    ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Block Script and Frames</title>
+</head>
+<body>
+    <form method="POST">
+        Website URL:
+        <input type="text"  name="url">
 
+        <p><input type="checkbox"  name="links[]" value="https://pagead2.googlesyndication.com/pagead/">Google ads</p>
+        <p><input type="checkbox"  name="links[]" value="https://www.google-analytics.com/analytics.js">Google Analytics</p>
+        <p><input type="checkbox"  name="links[]" value="https://connect.soundcloud.com/">Sound Cloud</p>
+        <p><input type="checkbox"  name="links[]" value="https://www.youtube.com/watch?v=RgD8sDUKn-g">YouTube Embed</p>
 
-
-
-    <body>
-    <form  method="post">
-        <p>Google Ads: <input type="checkbox" name="handleScripts[]" value="ads"></p>
-        <p>Google Analytics: <input type="checkbox" name="handleScripts[]" value="analytics"></p>
-        <p>Sound Cloud: <input type="checkbox" name="handleScripts[]" value="sound"></p>
-        <p>YouTube Embed: <input type="checkbox" name="handleScripts[]" value="youtube"></p>
-        <p>Custom URL : <input type="text" name="url">
+        <p >Custom Script/Iframe Pattern:
+        <input type="text" name="links[]"></p>
         <button>Submit</button>
-
-
     </form>
-    </body>
+</body>
 </html>
